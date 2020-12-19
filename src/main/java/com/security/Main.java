91130d48a0ba5f65e2116b7ccca6477fc2fce05b
hdMGGuSP <railwaytan@gmail.com>
2020-12-19 12:14:41 +0200

    added main

--- src/main/java/com/security/Main.java
+++ src/main/java/com/security/Main.java
@@ -1,9 +1,14 @@
 package com.security;
 
-import java.util.Random;
+import com.security.task1.LcgCracker;
+import com.security.task2.MtCracker;
+import com.security.task3.BetterMtCracker;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        LcgCracker.main(new String[]{});
+        MtCracker.main(new String[]{});
+        BetterMtCracker.main(new String[]{});
     }
 }
